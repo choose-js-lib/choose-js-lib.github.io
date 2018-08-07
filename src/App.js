859d65b47d890ea78662aacd0116ef9600b28273
Devin Chen <d3vin.chen@gmail.com>
2018-08-08 02:29:51 +0800

    Add libs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+import libs from "./libs";
 
 class App extends Component {
+  state = {
+    name: ""
+  };
+
+  handleSearch = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  filterFunc = lib =>
+    lib.name.toLowerCase().indexOf(this.state.name.toLowerCase()) !== -1;
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <input value={this.state.name} onChange={this.handleSearch} />
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {libs.filter(this.filterFunc).map(lib => (
+            <li key={lib.name}>
+              <a href={lib.link} target="_blank">
+                {lib.name}
+              </a>
+            </li>
+          ))}
         </p>
       </div>
     );
---
+++ src/libs.js
@@ -0,0 +1,19 @@
+const libs = [
+  {
+    name: "litegraph.js",
+    categories: ["editor", "node-editor"],
+    link: "https://github.com/jagenjo/litegraph.js"
+  },
+  {
+    name: "React",
+    categories: ["framework"],
+    link: "https://github.com/facebook/react"
+  },
+  {
+    name: "Angular",
+    categories: ["framework"],
+    link: "https://github.com/angular/angular"
+  }
+];
+
+export default libs;
